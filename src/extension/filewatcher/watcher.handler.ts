23f2796e2944de90f469c6f73e8a08e7c78171ad
Enliven26 <16521443@mahasiswa.itb.ac.id>
2024-10-12 19:10:14 +0700

    feat: file rename validation

--- src/extension/filewatcher/watcher.handler.ts
+++ src/extension/filewatcher/watcher.handler.ts
@@ -20,15 +20,15 @@ export const handleFileChangesAsync = async (context: vscode.ExtensionContext, e
 };
 
 const getFileChanges = (event: vscode.TextDocumentChangeEvent): Array<FileChangeModel> => {
-    var result = new Array<FileChangeModel>();
+    const result = new Array<FileChangeModel>();
 
     event.contentChanges.forEach(change => {
-        var text = change.text;
+        const text = change.text;
 
-        var lines = text.split(/\r\n|\r|\n/);
+        const lines = text.split(/\r\n|\r|\n/);
         
-        var startLine = change.range.start.line;
-        var endLine = Math.max(startLine + lines.length - 1, change.range.end.line);
+        const startLine = change.range.start.line;
+        const endLine = Math.max(startLine + lines.length - 1, change.range.end.line);
 
         result.push({ uri: event.document.uri.fsPath, startLine, endLine });
     });
--- src/extension/filewatcher/watcher.ts
+++ src/extension/filewatcher/watcher.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { validateFileChangesAsync } from './watcher.validation';
+import { validateFileChangesAsync, validateFileRenameAsync } from './watcher.validation';
 import { handleFileChangesAsync } from './watcher.handler';
 import { InMemoryState } from '../models';
 
@@ -7,11 +7,19 @@ export const watchFileChangesAsync = async (
     context: vscode.ExtensionContext, 
     event: vscode.TextDocumentChangeEvent,
     inMemoryState: InMemoryState) => {
-    var isValid = await validateFileChangesAsync(context, event, inMemoryState);
+    const isValid = await validateFileChangesAsync(context, event, inMemoryState);
 
     if (!isValid){
         return;
     }
     
     await handleFileChangesAsync(context, event);
+};
+
+export const watchFileRenameAsync = async (
+    context: vscode.ExtensionContext,
+    event: vscode.FileRenameEvent,
+    inMemoryState: InMemoryState) => {
+        const validationResultMap = await validateFileRenameAsync(context, event, inMemoryState);
+
 };
