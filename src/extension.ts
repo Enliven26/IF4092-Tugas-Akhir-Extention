1459de8ee08e60ba38bd1101326760ddbb598c9f
Enliven26 <16521443@mahasiswa.itb.ac.id>
2024-10-12 12:08:20 +0700

    feat: file watcher

--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 import { Commands } from './constants/enums';
 import { setApiKey, clearApiKey } from './commands/apikey';
 import { setUpGitHook } from './commands/githook';
+import path from 'path';
+import { watchFileChanges } from './filewatcher/watcher';
 
 const registerCommands = (context: vscode.ExtensionContext) => {
 	const disposables = [
@@ -13,8 +15,14 @@ const registerCommands = (context: vscode.ExtensionContext) => {
 	disposables.forEach(disposable => context.subscriptions.push(disposable));
 };
 
+const registerFileWatcher = (context: vscode.ExtensionContext) => {
+	const fileWatcher = vscode.workspace.onDidChangeTextDocument(watchFileChanges);
+    context.subscriptions.push(fileWatcher);
+};
+
 export function activate(context: vscode.ExtensionContext) {
 	registerCommands(context);
+	registerFileWatcher(context);
 }
 
 export function deactivate() {}
