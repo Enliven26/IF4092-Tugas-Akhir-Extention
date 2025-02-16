b532b42cd65067da972a9cf94ff68b72da1032e2
johanes lee <johaneslee26@gmail.com>
2025-02-16 18:05:49 +0700

    feat: set setting values in githook setup

--- src/extension/domain/constants/values.ts
+++ src/extension/domain/constants/values.ts
@@ -3,19 +3,12 @@ import * as path from 'path';
 export const backgroundTaskInterval = 10000;
 export const gitHookVenvFolderName = 'autocommit_venv';
 
-const gitFolderName = ".git";
-const gitHookFolderName = "hooks";
-
 export const projectHookFolderPath = "hooks";
-
-export const gitHookRelativeFolderPath = path.join(gitFolderName, gitHookFolderName);
-
+export const gitHookRelativeFolderPath = path.join(".git", "hooks");
 export const gitHookSetupLockRelativePath = path.join(gitHookRelativeFolderPath, 'autocommit_setup_lock');
-
 export const gitHookContextFolderRelativePath = path.join(gitHookRelativeFolderPath, 'autocommit_context');
-
 export const gitHookRequirementFileName = 'autocommit_requirements.txt';
+export const gitHookSettingsFileName = 'autocommit_settings.json';
 
 export const contextSeparator = "--- RETRIEVED DOCUMENT SPLIT END ---";
-
 export const contextFileName = 'context.txt';
